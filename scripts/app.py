27fee4196e9945cbdcd667c5b8029a8fbe80e4c7
nikowis <nikowis@gmail.com>
2018-08-01 20:43:36 +0200

    Teaching simple prediction models.

--- scripts/app.py
+++ scripts/app.py
@@ -1,5 +1,41 @@
+import math
+import numpy as np
 import quandl
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get("WIKI/GOOGL")
 
-print(df.head())
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+# extract attributes
+X = np.array(df.drop(['label'], 1))
+# extract labels
+y = np.array(df['label'])
+
+# scale from -1 to 1
+X = preprocessing.scale(X)
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+for k in ['linear','poly','rbf','sigmoid']:
+    clf = svm.SVR(kernel=k)
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print(k,confidence)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+print(confidence)
+
+
