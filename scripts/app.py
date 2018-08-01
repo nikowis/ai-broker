055a89f9051de2110fc649b13e376daa734efdfc
nikowis <nikowis@gmail.com>
2018-08-01 23:14:06 +0200

    Plotting simple prediction.

--- scripts/app.py
+++ scripts/app.py
@@ -3,8 +3,14 @@ import numpy as np
 import quandl
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+
 
 df = quandl.get("WIKI/GOOGL")
+
+print(df.tail())
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 
@@ -16,26 +22,41 @@ forecast_col = 'Adj. Close'
 df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
-# extract attributes
+
 X = np.array(df.drop(['label'], 1))
-# extract labels
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+
 y = np.array(df['label'])
 
-# scale from -1 to 1
-X = preprocessing.scale(X)
-
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
-
-for k in ['linear','poly','rbf','sigmoid']:
-    clf = svm.SVR(kernel=k)
-    clf.fit(X_train, y_train)
-    confidence = clf.score(X_test, y_test)
-    print(k,confidence)
-
-clf = LinearRegression()
+clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 confidence = clf.score(X_test, y_test)
-print(confidence)
 
+forecast_set = clf.predict(X_lately)
 
+print(forecast_set, confidence, forecast_out)
+
+style.use('ggplot')
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day_seconds = 86400
+next_unix = last_unix + one_day_seconds
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day_seconds
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
