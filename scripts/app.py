bde3f1eab0b30998179c569c6e991a51d81a558e
nikowis <nikowis@gmail.com>
2018-09-13 19:47:40 +0200

    Extracted model runner.

--- scripts/app.py
+++ scripts/app.py
@@ -1,11 +1,7 @@
-import math
-import numpy as np
-from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
-import datetime
-import matplotlib.pyplot as plt
-from matplotlib import style
 import scripts.alpha as stck
+import scripts.model_runner as model_runner
+import math
 
 alpha = stck.AlphaVantage()
 df = alpha.daily_adjusted('GOOGL')
@@ -21,45 +17,4 @@ df[HL_PCT] = (df[alpha.HIGH_COL] - df[alpha.LOW_COL]) / df[alpha.LOW_COL] * 100.
 df[PCT_CHANGE] = (df[alpha.ADJUSTED_CLOSE_COL] - df[alpha.OPEN_COL]) / df[alpha.OPEN_COL] * 100.0
 df = df[[alpha.ADJUSTED_CLOSE_COL, HL_PCT, PCT_CHANGE, alpha.VOLUME_COL]]
 
-forecast_col = alpha.ADJUSTED_CLOSE_COL
-df.fillna(value=-99999, inplace=True)
-forecast_out = int(math.ceil(0.01 * len(df)))
-df['label'] = df[forecast_col].shift(-forecast_out)
-
-X = np.array(df.drop(['label'], 1))
-X = preprocessing.scale(X)
-X_lately = X[-forecast_out:]
-X = X[:-forecast_out]
-
-df.dropna(inplace=True)
-
-y = np.array(df['label'])
-
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
-clf = LinearRegression(n_jobs=-1)
-clf.fit(X_train, y_train)
-confidence = clf.score(X_test, y_test)
-
-forecast_set = clf.predict(X_lately)
-
-print(forecast_set, confidence, forecast_out)
-
-style.use('ggplot')
-df['Forecast'] = np.nan
-
-last_date = df.iloc[-1].name
-last_unix = last_date.timestamp()
-one_day_seconds = 86400
-next_unix = last_unix + one_day_seconds
-
-for i in forecast_set:
-    next_date = datetime.datetime.fromtimestamp(next_unix)
-    next_unix += one_day_seconds
-    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
-
-df[alpha.ADJUSTED_CLOSE_COL].plot()
-df['Forecast'].plot()
-plt.legend(loc=4)
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.show()
+model_runner.predict(alpha.ADJUSTED_CLOSE_COL, LinearRegression(n_jobs=-1), df, int(math.ceil(0.01 * len(df))))
---
+++ scripts/model_runner.py
@@ -0,0 +1,48 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+
+def predict(forecast_col, predictor, dateframe, forecast_days):
+    dateframe.fillna(value=-99999, inplace=True)
+    dateframe['label'] = dateframe[forecast_col].shift(-forecast_days)
+
+    X = np.array(dateframe.drop(['label'], 1))
+    X = preprocessing.scale(X)
+    X_lately = X[-forecast_days:]
+    X = X[:-forecast_days]
+
+    dateframe.dropna(inplace=True)
+
+    y = np.array(dateframe['label'])
+
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+    predictor.fit(X_train, y_train)
+    confidence = predictor.score(X_test, y_test)
+
+    forecast_set = predictor.predict(X_lately)
+
+    print('Confidence {}'.format(confidence))
+
+    style.use('ggplot')
+    dateframe['Forecast'] = np.nan
+
+    last_date = dateframe.iloc[-1].name
+    last_unix = last_date.timestamp()
+    one_day_seconds = 86400
+    next_unix = last_unix + one_day_seconds
+
+    for i in forecast_set:
+        next_date = datetime.datetime.fromtimestamp(next_unix)
+        next_unix += one_day_seconds
+        dateframe.loc[next_date] = [np.nan for _ in range(len(dateframe.columns) - 1)] + [i]
+
+    dateframe[forecast_col].plot()
+    dateframe['Forecast'].plot()
+    plt.legend(loc=4)
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.show()
+    return dateframe
