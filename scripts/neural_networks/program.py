7d76ac146812a3e58c450d36048b457d4071a97e
Nikodem Wiśniewski <nikodem.wisniewski@3e.pl>
2018-09-25 14:10:05 +0200

    Neural network changed to keras implementation.

--- scripts/neural_networks/program.py
+++ scripts/neural_networks/program.py
@@ -1,11 +1,10 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 import os
-import neural_networks.feedforward_model as ffm
-import alpha
+
+import numpy as np
+import tensorflow as tf
 from sklearn import model_selection
+
+import alpha
 
 TICKER = 'GOOGL'
 
@@ -17,92 +16,38 @@ base_path = './../../target/neural_networks'
 if not os.path.exists(base_path):
     os.makedirs(base_path)
 
-data = df[[alpha.ADJUSTED_CLOSE_COL]]
-df = df[[alpha.ADJUSTED_CLOSE_COL]]
-
-# Dimensions of dataset
-n = data.shape[0]
-p = data.shape[1]
-
-# Make data a np.array
-
+mnist = tf.keras.datasets.mnist
 forecast_days = 1
 df.dropna(inplace=True)
 df.fillna(value=-99999, inplace=True)
+df = df[[alpha.ADJUSTED_CLOSE_COL]]
 df[alpha.LABEL_COL] = df[alpha.ADJUSTED_CLOSE_COL].shift(-forecast_days)
 
 X = np.array(df.drop([alpha.LABEL_COL], 1))
 # X = preprocessing.scale(X)
 X_lately = X[-forecast_days:]
 X = X[:-forecast_days]
+
 df_removed = df.dropna()
 
 y = np.array(df_removed[alpha.LABEL_COL])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
-# data = data.values
-# # Training and test data
-# train_start = 0
-# train_end = int(np.floor(0.8 * n))
-# test_start = train_end + 1
-# test_end = n
-# data_train = data[np.arange(train_start, train_end), :]
-# data_test = data[np.arange(test_start, test_end), :]
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(8, activation=tf.nn.relu),
+    tf.keras.layers.Dense(4, activation=tf.nn.relu),
+    tf.keras.layers.Dense(1, activation=tf.nn.relu)
+])
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['accuracy'])
 
-# # Scale data
-# scaler = MinMaxScaler(feature_range=(-1, 1))
-# scaler.fit(data_train)
-# data_train = scaler.transform(data_train)
-# data_test = scaler.transform(data_test)
-#
-# # Build X and y
-# X_train = data_train[:, 1:]
-# y_train = data_train[:, 0]
-# X_test = data_test[:, 1:]
-# y_test = data_test[:, 0]
+model.fit(X_train, y_train, epochs=10)
+accuracy = model.evaluate(X_test, y_test)
 
-model, opt, mse, out, X_plchldr, Y_plchldr = ffm.construct_model(X_train)
+print("Accuracy: ", accuracy)
 
-# Setup plot
-plt.ion()
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-line1, = ax1.plot(y_test)
-line2, = ax1.plot(y_test * 0.5)
-plt.show()
+a = model.predict(X_lately)
 
-# Fit neural model
-batch_size = 256
-mse_train = []
-mse_test = []
-
-# Run
-epochs = 10
-for e in range(epochs):
-
-    # Shuffle training data
-    shuffle_indices = np.random.permutation(np.arange(len(y_train)))
-    X_train = X_train[shuffle_indices]
-    y_train = y_train[shuffle_indices]
-
-    # Minibatch training
-    for i in range(0, len(y_train) // batch_size):
-        start = i * batch_size
-        batch_x = X_train[start:start + batch_size]
-        batch_y = y_train[start:start + batch_size]
-        # Run optimizer with batch
-        model.run(opt, feed_dict={X_plchldr: batch_x, Y_plchldr: batch_y})
-
-        # Show progress
-        if np.mod(i, 50) == 0:
-            # MSE train and test
-            mse_train.append(model.run(mse, feed_dict={X_plchldr: X_train, Y_plchldr: y_train}))
-            mse_test.append(model.run(mse, feed_dict={X_plchldr: X_test, Y_plchldr: y_test}))
-            print('MSE Train: ', mse_train[-1])
-            print('MSE Test: ', mse_test[-1])
-            # Prediction
-            pred = model.run(out, feed_dict={X_plchldr: X_test})
-            line2.set_ydata(pred)
-            plt.title('Epoch ' + str(e) + ', Batch ' + str(i))
-            plt.pause(0.01)
+print('Predicted value ', a)
