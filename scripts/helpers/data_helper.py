38aded2b7cecb996a50d5a67dbbfd3db0511c165
nikowis <nikowis@gmail.com>
2018-11-15 22:31:53 +0100

    Refactored variables.

--- scripts/helpers/data_helper.py
+++ scripts/helpers/data_helper.py
@@ -12,41 +12,43 @@ def extract_data(df, forecast_days=1, history_days=0):
 
     :param df: dateframe data
     :param forecast_days: how many days to forecast out
+    :param history_days: how many days from history to put in each row
     :return:
-        (df, X, y, X_lately) # tuple of (modified df, learning data, labels, data without labels)
+        (df, x_standardized, x_train, x_test, y_train_binary, y_test_binary) # tuple of (modified df, standardized x,
+        learning x, testing x, learning y (one hot), test y (one hot))
     """
     df[const.HL_PCT_CHANGE_COL] = (df[const.HIGH_COL] - df[const.LOW_COL]) / df[const.HIGH_COL] * 100
-    X = np.array(df[[const.VOLUME_COL, const.ADJUSTED_CLOSE_COL, const.HL_PCT_CHANGE_COL]])
+    x = np.array(df[[const.VOLUME_COL, const.ADJUSTED_CLOSE_COL, const.HL_PCT_CHANGE_COL]])
     if history_days > 0:
-        input_rows = X.shape[0]
-        input_columns = X.shape[1]
-        extended_X = np.zeros((input_rows, input_columns + history_days * input_columns))
-        extended_X[:, :] = np.nan
-        extended_X[:, 0:input_columns] = X
+        input_rows = x.shape[0]
+        input_columns = x.shape[1]
+        extended_x = np.zeros((input_rows, input_columns + history_days * input_columns))
+        extended_x[:, :] = np.nan
+        extended_x[:, 0:input_columns] = x
         for i in range(1, history_days + 1):
-            extended_X[i:, i * input_columns:(i + 1) * input_columns] = X[:input_rows - i, :]
-        nan_row_count = np.count_nonzero(np.isnan(extended_X).any(axis=1))
-        X = extended_X[~np.isnan(extended_X).any(axis=1)]
+            extended_x[i:, i * input_columns:(i + 1) * input_columns] = x[:input_rows - i, :]
+        nan_row_count = np.count_nonzero(np.isnan(extended_x).any(axis=1))
+        x = extended_x[~np.isnan(extended_x).any(axis=1)]
         df = df[nan_row_count:]
 
-    X_lately = X[-forecast_days:]
-    X = X[:-forecast_days]
+    x_lately = x[-forecast_days:]
+    x = x[:-forecast_days]
     df = df[:-forecast_days]
     y = np.array(df[const.LABEL_DISCRETE_COL])
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-    X_standarized, X_test, X_train = standardize(X, X_test, X_train)
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
+    x_standardized, x_test, x_train = standardize(x, x_test, x_train)
     y_train_binary = keras.utils.to_categorical(y_train)
     y_test_binary = keras.utils.to_categorical(y_test)
-    return df, X_standarized, X_train, X_test, y_train_binary, y_test_binary
+    return df, x_standardized, x_train, x_test, y_train_binary, y_test_binary
 
 
-def standardize(X, X_test, X_train):
-    # standarize
-    if len(X_train.shape) == 1:
-        X_train = np.expand_dims(X_train, axis=1)
-        X_test = np.expand_dims(X_test, axis=1)
-    std_scale = StandardScaler().fit(X_train)
-    X_train = std_scale.transform(X_train)
-    X_test = std_scale.transform(X_test)
-    X_standarized = std_scale.transform(X)
-    return X_standarized, X_test, X_train
+def standardize(x, x_test, x_train):
+    # standardize
+    if len(x_train.shape) == 1:
+        x_train = np.expand_dims(x_train, axis=1)
+        x_test = np.expand_dims(x_test, axis=1)
+    std_scale = StandardScaler().fit(x_train)
+    x_train = std_scale.transform(x_train)
+    x_test = std_scale.transform(x_test)
+    x_standardized = std_scale.transform(x)
+    return x_standardized, x_test, x_train
--- scripts/neural_networks/nn_runner.py
+++ scripts/neural_networks/nn_runner.py
@@ -14,7 +14,7 @@ if not os.path.exists(base_path):
     os.makedirs(base_path)
 
 LAYERS = [20, 20, 20]
-EPOCHS = 100
+EPOCHS = 5
 FORECAST_DAYS = 1
 ACTIVATION = None
 OPTIMIZER = 'adam'
@@ -29,19 +29,20 @@ def run(layers=LAYERS, epochs=EPOCHS, forecast_days=FORECAST_DAYS, activation=AC
     db_conn = db_access.create_db_connection()
     df = db_access.find_one_by_ticker_dateframe(db_conn, ticker)
 
-    df, X_standarized, X_train, X_test, y_train_binary, y_test_binary = data_helper.extract_data(df, forecast_days,
+    df, x_standarized, x_train, x_test, y_train_binary, y_test_binary = data_helper.extract_data(df, forecast_days,
                                                                                                  history_days=history_days)
 
-    model = nn_model.create_seq_model(layers, input_size=X_train.shape[1], activation=activation, optimizer=optimizer,
+    model = nn_model.create_seq_model(layers, input_size=x_train.shape[1], activation=activation, optimizer=optimizer,
                                       loss=loss)
 
-    history = model.fit(X_train, y_train_binary, epochs=epochs, batch_size=batch_size)
-    loss, accuracy = model.evaluate(X_test, y_test_binary)
+    history = model.fit(x_train, y_train_binary, epochs=epochs, batch_size=batch_size)
+    loss, accuracy = model.evaluate(x_test, y_test_binary)
 
     print("Loss: ", loss, " Accuracy: ", accuracy, " epochs: ", epochs)
-    predicted_binary = model.predict(X_standarized)
+    predicted_binary = model.predict(x_standarized)
     predicted = [np.argmax(pred, axis=None, out=None) for pred in predicted_binary]
     df[const.FORECAST_DISCRETE_COL] = predicted
+
     main_title = "Loss: " + str(round(loss, 4)) + ", accuracy: " + str(round(accuracy, 4)) + ", epochs: " + str(
         epochs) + ', history days:' + str(history_days) + '\n'
     main_title += 'Layers: [' + ''.join(
