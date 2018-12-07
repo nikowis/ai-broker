ecef8fb7a56479b4c24df0e78594b2b7c41fdd01
nikowis <nikowis@gmail.com>
2018-12-07 01:00:28 +0100

    Added timer.

--- scripts/neural_networks/nn_program.py
+++ scripts/neural_networks/nn_program.py
@@ -1,11 +1,12 @@
 from db import db_access
 from helpers import data_helper
 from neural_networks import nn_runner
+import time
 
 
 def main():
     ticker = 'CALM'
-    db_conn = db_access.create_db_connection(remote=True)
+    db_conn = db_access.create_db_connection(remote=False)
     df = db_access.find_one_by_ticker_dateframe(db_conn, ticker)
 
     epochs = 200
@@ -16,10 +17,10 @@ def main():
               # 'categorical_crossentropy'
               ]
 
-    activations = ['softmax', 'relu']
+    activations = ['relu']
 
-    optimizers = ['adam', 'sgd', 'rmsprop']
-
+    optimizers = ['adam', 'sgd']
+    total_time = time.time()
     iteration = 0
     for hist_dayz in range(0, 50, 1):
         df, x_standarized, x_train, x_test, y_train_binary, y_test_binary = data_helper.extract_data(df, 1, hist_dayz)
@@ -33,10 +34,12 @@ def main():
                                 actv) + '_OPTIMIZER_' + str(
                                 optmzr) + '_HIST_' + str(hist_dayz)
                             print('\nSTARTING TRAINING FOR ' + file_name)
+                            iter_time = time.time()
                             nn_runner.run(df, x_standarized, x_train, x_test, y_train_binary, y_test_binary,
                                           epochs=epochs,
                                           layers=layers, optimizer=optmzr, loss_fun=lss, activation=actv,
                                           history_days=hist_dayz, file_name=file_name, outstanding_treshold=0.40)
+                            print('Total time ', str(int(time.time()-total_time)), 's, iteration '+str(iteration)+' time ', str(int(time.time() - iter_time)), 's.')
 
 
 def is_bad_combination(actv, optmzr):
