9b5299c75f13aeff1fc6a24fa6e8eec45bcc15cc
nikowis <nikowis@gmail.com>
2018-12-09 00:29:10 +0100

    ROC & AUC.

--- scripts/helpers/plot_helper.py
+++ scripts/helpers/plot_helper.py
@@ -1,7 +1,9 @@
 import os
+from itertools import cycle
 
 import matplotlib.pyplot as plt
 from matplotlib import style
+from sklearn.metrics import roc_curve, auc
 
 import db.stock_constants as const
 
@@ -25,6 +27,10 @@ RATE_CHANGE_LABEL = 'Zmiana kursu'
 RISE_LABEL = 'wzrost'
 IDLE_LABEL = 'utrzymanie'
 FALL_LABEL = 'spadek'
+CLASS_LABELS = [FALL_LABEL, IDLE_LABEL, RISE_LABEL]
+PREDICTED_LEGEND = 'Przewidywane'
+REAL_LEGEND = 'Rzeczywiste'
+
 REAL_VALUES_LABEL = 'Rzeczywiste wartości '
 PREDICTED_VALUES_LABEL = 'Przewidywane wartości '
 EPOCH_LABEL = 'iteracja'
@@ -35,6 +41,13 @@ ACCURACY_TITLE = 'Dokładność'
 TEST_DATA = 'Dane testowe'
 TRAIN_DATA = 'Dane treningowe'
 
+#ROC
+FPR_LABEL = 'False Positive Rate'
+TPR_LABEL = 'True Positive Rate'
+ROC_TITLE = 'Krzywe ROC'
+MICRO_ROC_KEY = "micro"
+CLASS_ROC_LABEL = "Klasa '{0}' (obszar = {1:0.2f})"
+MICRO_AVG_ROC_LABEL = 'Mikro-średnia klas (obszar = {0:0.2f})'
 
 def legend_labels_save_files(title, file_name='img', base_img_path=BASE_IMG_PATH, xlabel=DATE_LABEL,
                              ylabel=CLOSE_PRICE_USD_LABEL, legend=4):
@@ -49,25 +62,53 @@ def legend_labels_save_files(title, file_name='img', base_img_path=BASE_IMG_PATH
     plt.close()
 
 
-def plot_result(df, history, main_title, file_name, outstanding=False):
-    fig = plt.figure(figsize=(12, 12))
-    style.use('ggplot')
+def calculate_roc_auc(y_test, y_test_score, classes_count):
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(classes_count):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_test_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
 
+    fpr[MICRO_ROC_KEY], tpr[MICRO_ROC_KEY], _ = roc_curve(y_test.ravel(), y_test_score.ravel())
+    roc_auc[MICRO_ROC_KEY] = auc(fpr[MICRO_ROC_KEY], tpr[MICRO_ROC_KEY])
+
+    return fpr, tpr, roc_auc
+
+
+def plot_result(df,y_test, y_test_score, classes_count, history, main_title, file_name, outstanding=False):
+    fpr, tpr, roc_auc = calculate_roc_auc(y_test,y_test_score, classes_count)
+
+    plt.figure(figsize=(12, 12))
+    style.use('ggplot')
     plt.suptitle(main_title)
+
     plt.subplot(2, 2, 1)
     df[const.LABEL_DISCRETE_COL].plot(kind='hist', xticks=[0, 1, 2],alpha=0.7, label=RATE_CHANGE_LABEL)
     df[const.FORECAST_DISCRETE_COL].plot(kind='hist', xticks=[0, 1, 2],alpha=0.7, label=RATE_CHANGE_FORECAST_LABEL)
-    plt.legend(['Rzeczywiste', 'Przewidywane'], loc='upper left')
-    plt.xticks([0, 1, 2], [FALL_LABEL, IDLE_LABEL, RISE_LABEL])
+    plt.legend([REAL_LEGEND, PREDICTED_LEGEND], loc='upper left')
+    plt.xticks([0, 1, 2], CLASS_LABELS)
     plt.xlabel(VALUE_CHANGE_LABEL)
     plt.ylabel(FORECAST_COUNT_LABEL)
     plt.title(REAL_VALUES_LABEL)
+
     plt.subplot(2, 2, 2)
-    df[const.FORECAST_DISCRETE_COL].plot(kind='hist', xticks=[0, 1, 2], label=RATE_CHANGE_FORECAST_LABEL)
-    plt.xticks([0, 1, 2], [FALL_LABEL, IDLE_LABEL, RISE_LABEL])
-    plt.xlabel(VALUE_CHANGE_LABEL)
-    plt.ylabel(FORECAST_COUNT_LABEL)
-    plt.title(PREDICTED_VALUES_LABEL)
+    plt.plot(fpr[(MICRO_ROC_KEY)], tpr[MICRO_ROC_KEY],
+             label=MICRO_AVG_ROC_LABEL.format(roc_auc[MICRO_ROC_KEY]),
+             color='red', linewidth=3)
+
+    colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+    for i, color in zip(range(classes_count), colors):
+        plt.plot(fpr[i], tpr[i], color=color, lw=2,
+                 label=CLASS_ROC_LABEL.format(CLASS_LABELS[i], roc_auc[i]))
+    plt.plot([0, 1], [0, 1], 'k--', lw=2)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel(FPR_LABEL)
+    plt.ylabel(TPR_LABEL)
+    plt.title(ROC_TITLE)
+    plt.legend(loc="lower right")
+
     plt.subplot(2, 2, 3)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -75,6 +116,7 @@ def plot_result(df, history, main_title, file_name, outstanding=False):
     plt.ylabel(LOSS_LABEL)
     plt.xlabel(EPOCH_LABEL)
     plt.legend([TRAIN_DATA, TEST_DATA], loc='upper left')
+
     plt.subplot(2, 2, 4)
     plt.plot(history.history['categorical_accuracy'])
     plt.plot(history.history['val_categorical_accuracy'])
@@ -92,5 +134,5 @@ def plot_result(df, history, main_title, file_name, outstanding=False):
     else:
         # plt.savefig('{}/{}.eps'.format(BASE_IMG_PATH, file_name), format='eps', dpi=1000)
         plt.savefig('{}/{}.png'.format(BASE_IMG_PATH, file_name))
-    #plt.show()
+    plt.show()
     plt.close()
--- scripts/neural_networks/nn_runner.py
+++ scripts/neural_networks/nn_runner.py
@@ -16,22 +16,22 @@ TICKER = 'GOOGL'
 HISTORY_DAYS = 0
 
 
-def run(df, x_standarized, x_train, x_test, y_train_binary, y_test_binary, layers=LAYERS, epochs=EPOCHS,
+def run(df, x, x_train, x_test, y_train_binary, y_test_binary, layers=LAYERS, epochs=EPOCHS,
         activation=ACTIVATION, optimizer=OPTIMIZER,
         loss_fun=LOSS_FUN, batch_size=BATCH_SIZE, history_days=HISTORY_DAYS, file_name='model_stats',
         outstanding_treshold=0.42):
+    _, classes_count = y_test_binary.shape
     model = nn_model.create_seq_model(layers, input_size=x_train.shape[1], activation=activation, optimizer=optimizer,
-                                      loss=loss_fun)
+                                      loss=loss_fun, class_count=classes_count)
 
-    history = model.fit(x_train, y_train_binary,validation_data=(x_test, y_test_binary), epochs=epochs, batch_size=batch_size, verbose=0)
+    history = model.fit(x_train, y_train_binary, validation_data=(x_test, y_test_binary), epochs=epochs,
+                        batch_size=batch_size, verbose=0)
     loss, accuracy = model.evaluate(x_test, y_test_binary, verbose=0)
-
     print("Loss: ", loss, " Accuracy: ", accuracy, " epochs: ", epochs)
-    predicted_binary = model.predict(x_standarized)
-    predicted = [np.argmax(pred, axis=None, out=None) for pred in predicted_binary]
+    predicted = model.predict_classes(x)
     df[const.FORECAST_DISCRETE_COL] = predicted
     count_matching = np.count_nonzero(np.where(df[const.FORECAST_DISCRETE_COL] == df[const.LABEL_DISCRETE_COL], 1, 0))
-    print('Accuracy on full data:', count_matching/len(predicted))
+    print('Accuracy on full data:', count_matching / len(predicted))
 
     main_title = "Loss: " + str(round(loss, 4)) + ", accuracy: " + str(round(accuracy, 4)) + ", epochs: " + str(
         epochs) + ', history days:' + str(history_days) + '\n'
@@ -39,8 +39,10 @@ def run(df, x_standarized, x_train, x_test, y_train_binary, y_test_binary, layer
         str(e) + " " for e in layers) + '], optimizer: ' + optimizer + ', loss: ' + str(
         loss_fun) + ', activation: ' + str(
         activation)
+    y_test_score = model.predict(x_test)
 
-    plot_helper.plot_result(df, history, main_title, file_name, accuracy >= outstanding_treshold)
+    plot_helper.plot_result(df, y_test_binary, y_test_score, classes_count, history, main_title, file_name,
+                            accuracy >= outstanding_treshold)
 
 
 if __name__ == '__main__':
