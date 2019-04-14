561bbceb72d3a1eeb422bb29541300b3f12a7ab2
nikowis <nikowis@gmail.com>
2019-04-14 16:56:15 +0200

    NN testing with new technical indicators.

--- scripts/nn_program.py
+++ scripts/nn_program.py
@@ -4,18 +4,28 @@ import data_helper
 import db_access
 import nn_model
 import plot_helper
+import stock_constants as const
 
 MIN_DATE = '2009-01-01'
 
+SELECTED_SYM = 'USLM'
+
+
 def main():
-    ticker = 'ACUR'
     db_conn = db_access.create_db_connection(remote=False)
-    df_list, sym_list = db_access.find_by_tickers_to_dateframe_parse_to_df_list(db_conn, [ticker], min_date=MIN_DATE)
+    df_list, sym_list = db_access.find_by_tickers_to_dateframe_parse_to_df_list(db_conn, [SELECTED_SYM],
+                                                                                min_date=MIN_DATE)
     df = df_list[0]
-    epochs = 50
-    batch_size=10
-    layers = [4, 4, 4]
+    epochs = 200
+    batch_size = 10
+
     skip_iterations = 0
+
+    COLUMNS = [const.VOLUME_COL, const.OPEN_COL, const.ADJUSTED_CLOSE_COL, const.HIGH_COL, const.LOW_COL,
+               const.HL_PCT_CHANGE_COL, const.SMA_5_COL, const.SMA_10_COL, const.SMA_20_COL, const.EMA_12_COL,
+               const.EMA_26_COL, const.STOCH_K_COL, const.STOCH_D_COL, const.ROC_10_COL, const.TR_COL, const.MOM_6_COL,
+               const.MOM_12_COL, const.WILLR_5_COL, const.WILLR_10_COL, const.APO_6_COL, const.APO_12_COL,
+               const.RSI_6_COL, const.RSI_12_COL]
 
     # 'mean_squared_error', 'logcosh', 'categorical_crossentropy', 'binary_crossentropy'
     losses = ['binary_crossentropy']
@@ -25,43 +35,54 @@ def main():
 
     # 'sgd', 'adam', 'rmsprop'
     optimizers = ['adam']
+    cols_count = len(COLUMNS)
 
+    half = int(cols_count / 2)
+    third = int(cols_count / 3)
+    quarter = int(cols_count / 4)
+    sixth = int(cols_count / 6)
+    eight = int(cols_count / 8)
+
+    layers = [[eight, eight, eight],[sixth, sixth, sixth], [quarter, quarter, quarter],[third, third, third], [half, half, half],
+              [cols_count, cols_count, cols_count]]
     total_time = time.time()
     iteration = 0
-    for hist_dayz in range(0, 3, 1):
+    for hist_dayz in range(0, 1, 1):
+
         df_modified, x_standardized, x_train, x_test, y_train_one_hot, y_test_one_hot = data_helper.extract_data(df,
                                                                                                                  hist_dayz,
-                                                                                                                 binary_classification=True)
+                                                                                                                 binary_classification=True,
+                                                                                                                 input_columns=COLUMNS)
         for optmzr in optimizers:
-            for actv in activations:
-                for lss in losses:
-                    iteration += 1
-                    if iteration > skip_iterations:
-                        file_name = get_report_file_name(actv, hist_dayz, iteration, lss, optmzr)
-                        print('\nSTARTING TRAINING FOR ' + file_name)
-                        neuron_count = int(x_train.shape[1]/2)+1
-                        layers = [neuron_count, neuron_count, neuron_count]
-                        iter_time = time.time()
+            for lay in layers:
+                for actv in activations:
+                    for lss in losses:
+                        iteration += 1
+                        if iteration > skip_iterations:
+                            file_name = get_report_file_name(actv, hist_dayz, iteration, lss, optmzr)
+                            print('\nSTARTING TRAINING FOR ' + file_name)
 
-                        _, classes_count = y_test_one_hot.shape
-                        model = nn_model.create_seq_model(layers, input_size=x_train.shape[1], activation=actv,
-                                                          optimizer=optmzr,
-                                                          loss=lss, class_count=classes_count)
+                            iter_time = time.time()
 
-                        history = model.fit(x_train, y_train_one_hot, validation_data=(x_test, y_test_one_hot),
-                                            epochs=epochs,
-                                            batch_size=batch_size, verbose=0)
-                        loss, accuracy = model.evaluate(x_test, y_test_one_hot, verbose=0)
-                        print("Loss: ", loss, " Accuracy: ", accuracy, " epochs: ", epochs)
+                            _, classes_count = y_test_one_hot.shape
+                            model = nn_model.create_seq_model(lay, input_size=x_train.shape[1], activation=actv,
+                                                              optimizer=optmzr,
+                                                              loss=lss, class_count=classes_count)
 
-                        main_title = get_report_title(accuracy, actv, epochs, hist_dayz, layers, loss, lss, optmzr)
-                        y_test_score = model.predict(x_test)
+                            history = model.fit(x_train, y_train_one_hot, validation_data=(x_test, y_test_one_hot),
+                                                epochs=epochs,
+                                                batch_size=batch_size, verbose=0)
+                            loss, accuracy = model.evaluate(x_test, y_test_one_hot, verbose=0)
+                            print("Loss: ", loss, " Accuracy: ", accuracy, " epochs: ", epochs)
 
-                        plot_helper.plot_result(y_test_one_hot, y_test_score, classes_count, history, main_title,
-                                                file_name)
+                            main_title = get_report_title(accuracy, actv, epochs, hist_dayz, lay, loss, lss, optmzr)
+                            y_test_score = model.predict(x_test)
 
-                        print('Total time ', str(int(time.time() - total_time)),
-                              's, iteration ' + str(iteration) + ' time ', str(int(time.time() - iter_time)), 's.')
+                            plot_helper.plot_result(y_test_one_hot, y_test_score, classes_count, history, main_title,
+                                                    file_name)
+
+                            print('Total time ', str(int(time.time() - total_time)),
+                                  's, iteration ' + str(iteration) + ' time ', str(int(time.time() - iter_time)), 's.')
 
 
 def get_report_title(accuracy, actv, epochs, hist_dayz, layers, loss, lss, optmzr):
@@ -79,8 +100,6 @@ def get_report_file_name(actv, hist_dayz, iteration, lss, optmzr):
     return str(iteration) + '_LOSS_' + str(lss) + '_ACTIVATION_' + str(
         actv) + '_OPTIMIZER_' + str(
         optmzr) + '_HIST_' + str(hist_dayz)
-
-
 
 
 if __name__ == '__main__':
