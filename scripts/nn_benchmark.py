bff53a783b192869965584bf8ae14e1beb217459
nikowis <nikowis@gmail.com>
2019-06-12 21:05:34 +0200

    [code] Fixed roc for walk forward learning.

--- scripts/nn_benchmark.py
+++ scripts/nn_benchmark.py
@@ -3,6 +3,7 @@ import os
 import time
 from shutil import copyfile
 
+import keras
 import numpy as np
 import pandas as pd
 from keras.callbacks import EarlyStopping, ModelCheckpoint
@@ -15,6 +16,7 @@ import data_preprocessing
 import nn_model
 import plot_helper
 
+SATYSFYING_TRESHOLD = 0.85
 ROC_AUC_COL = 'roc_auc'
 ACC_COL = 'accuracy'
 TRAIN_TIME_COL = 'train_time'
@@ -73,7 +75,7 @@ def run(x_train, x_test, y_train, y_test, bench_params, results_df: pd.DataFrame
             mode='max')
 
         callbacks = [earlyStopping, mcp_save]
-        model, history, accuracy, loss = learn(model, learning_params, callbacks, model_path, x_train, x_test,
+        model, history, accuracy, loss, y_test_prediction = learn(model, learning_params, callbacks, model_path, x_train, x_test,
                                                y_train, y_test, verbose)
 
         if (binary_classification and accuracy < 0.6) or ((not binary_classification) and accuracy < 0.45):
@@ -102,28 +104,29 @@ def run(x_train, x_test, y_train, y_test, bench_params, results_df: pd.DataFrame
             round(loss, 4), round(accuracy, 4), number_of_epochs_it_ran, ''.join(
                 str(e) + " " for e in bench_params.model_params.layers))
 
-        if not learning_params.walk_forward_testing :
-            y_test_prediction = model.predict(x_test)
-            fpr, tpr, roc_auc = plot_helper.plot_result(y_test, y_test_prediction, bench_params, history, main_title,
+        if learning_params.walk_forward_testing:
+            y_test = np.concatenate(y_test)
+
+        fpr, tpr, roc_auc = plot_helper.plot_result(y_test, y_test_prediction, bench_params, history, main_title,
                                                         'nn-{0}-{1}'.format(learning_params.id, curr_iter_num))
-        else:
-            roc_auc=0
 
         results_df = results_df.append(
             {ID_COL: learning_params.id, EPOCHS_COL: int(number_of_epochs_it_ran), TRAIN_TIME_COL: iter_time,
              ACC_COL: accuracy,
              ROC_AUC_COL: roc_auc}, ignore_index=True)
 
-    print('ID {0} avg loss {1} avg accuracy {2} total time {3} s.'.format(learning_params.id, round(np.mean(losses), 4),
-                                                                          round(np.mean(accuracies), 4),
+    rounded_accuracy_mean = round(np.mean(accuracies), 4)
+    rounded_loss_mean = round(np.mean(losses), 4)
+    print('ID {0} avg loss {1} avg accuracy {2} total time {3} s.'.format(learning_params.id, rounded_loss_mean,
+                                                                          rounded_accuracy_mean,
                                                                           round(time.time() - total_time, 2)))
-
+    if rounded_accuracy_mean > SATYSFYING_TRESHOLD:
+        print('=============================================================================================')
     max_index = np.argmax(accuracies)
     best_model_of_all_path = best_model_paths[max_index]
     copyfile(best_model_of_all_path,
              '{0}nn_weights-{1}-best-accuracy-{2}.hdf5'.format(SAVE_MODEL_PATH, learning_params.id,
                                                                round(max(accuracies), 4)))
-
     return results_df
 
 
@@ -132,7 +135,8 @@ def learn(model, learning_params, callbacks, model_path, x_train, x_test, y_trai
         walk_iterations = len(x_train)
         walk_losses = []
         walk_accuracies = []
-
+        walk_y_test_prediction = []
+        walk_history = keras.callbacks.History()
         for walk_it in range(0, walk_iterations):
             walk_x_train = x_train[walk_it]
             walk_x_test = x_test[walk_it]
@@ -147,11 +151,13 @@ def learn(model, learning_params, callbacks, model_path, x_train, x_test, y_trai
                                 epochs=epochs, batch_size=learning_params.batch_size,
                                 callbacks=callbacks, verbose=0)
             ls, acc = model.evaluate(walk_x_test, walk_y_test, verbose=0)
+            y_test_prediction = model.predict(walk_x_test)
             walk_losses.append(ls)
             walk_accuracies.append(acc)
+            walk_y_test_prediction += y_test_prediction.tolist()
         if verbose:
             print('Walk accuracies: [{0}]'.format(walk_accuracies))
-        return model, history, np.mean(walk_accuracies), np.mean(walk_losses)
+        return model, history, np.mean(walk_accuracies), np.mean(walk_losses), np.array(walk_y_test_prediction)
     else:
         history = model.fit(x_train, y_train, validation_data=(x_test, y_test),
                             epochs=learning_params.epochs, batch_size=learning_params.batch_size,
@@ -159,7 +165,8 @@ def learn(model, learning_params, callbacks, model_path, x_train, x_test, y_trai
         # restores best epoch of this iteration
         model = load_model(model_path)
         loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
-        return model, history, accuracy, loss
+        y_test_prediction = model.predict(x_test)
+        return model, history, accuracy, loss, y_test_prediction
 
 
 if __name__ == '__main__':
@@ -167,16 +174,17 @@ if __name__ == '__main__':
     df = df_list[0]
 
     bench_params = benchmark_params.default_params(binary_classification=True)
-    bench_params.learning_params.iterations=2
+    bench_params.learning_params.iterations=1
     bench_params.learning_params.walk_forward_testing=True
     bench_params.preprocessing_params.walk_forward_testing=True
 
     param_grid = {
-        'epochs': [10, 20, 30, 40, 50, 70],
+        # 'epochs': [20, 30, 40, 50, 70],
+        'epochs': [3],
         'layers': [[], [2], [5]],
         'walk_forward_retrain_epochs':[1, 2, 3, 5, 10],
         'walk_forward_max_train_window_size':[None, 2000, 1000, 500, 300, 150],
-        'walk_forward_test_window_size':[200, 100, 50, 20, 10]
+        'walk_forward_test_window_size':[20, 15 ,10]
     }
     grid = ParameterGrid(param_grid)
 
@@ -190,7 +198,9 @@ if __name__ == '__main__':
         df, x, y, x_train, x_test, y_train, y_test = data_preprocessing.preprocess(df,
                                                                                    bench_params.preprocessing_params)
 
-        results_df = run(x_train, x_test, y_train, y_test, bench_params, results_df, verbose=True)
+        results_df = run(x_train, x_test, y_train, y_test, bench_params, results_df, verbose=False)
+
+        break
 
     if results_df is not None and len(results_df) > 0:
         results_df.to_csv('{0}results.csv'.format(SAVE_MODEL_PATH), index=False)
