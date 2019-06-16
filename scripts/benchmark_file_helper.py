62a9ad2bf591b3fcbc8ee088d77b33eff2cc446d
nikowis <nikowis@gmail.com>
2019-06-16 22:08:01 +0200

    [code] Fixed creating directory for result.csv.

--- scripts/benchmark_file_helper.py
+++ scripts/benchmark_file_helper.py
@@ -6,6 +6,8 @@ from benchmark_params import BenchmarkParams
 
 
 def initialize_dirs(bench_params: BenchmarkParams):
+    if not os.path.exists(bench_params.benchmark_path):
+        os.makedirs(bench_params.benchmark_path)
     if bench_params.save_files and not os.path.exists(bench_params.save_model_path):
         os.makedirs(bench_params.save_model_path)
     if bench_params.save_files and not os.path.exists(bench_params.save_img_path):
